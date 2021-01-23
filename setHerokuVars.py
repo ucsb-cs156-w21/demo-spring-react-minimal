3dfef9f6fdb979ef87d9ea74361a9cf38c920992
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-01-22 16:11:05 -0800

    pc - improvements to setHerokuVars.py
    * Improved error handling
    * Streamlined output

--- setHerokuVars.py
+++ setHerokuVars.py
@@ -8,15 +8,16 @@ def setVar(key, value,app_name):
   os.system(f'heroku config:set {key}={value} --app {app_name}')
 
 
-def setVarsFromFile(filename,app_name):  
+def setVarsFromFile(filename,app_name,message):  
   if not os.path.exists(filename):
-    print("No file found for: ",filename)
+    print("No file found for: ",filename,message)
     return
 
   with open(filename) as f:
     lines = f.read().split('\n')
-    print("Contents from ",filename,":")
-    print("\n".join(lines))
+    #print("Contents from ",filename,":")
+    #print("\n".join(lines))
+    print("Setting variables from ",filename,"...")
     for line in lines:
       try:
         [key, value] = line.split('=')
@@ -35,8 +36,8 @@ def main():
   print(f'heroku config:set SPRING_PROPERTIES="$(cat secrets-heroku.properties)" "$@" --app {app_name}')
   os.system(f'heroku config:set SPRING_PROPERTIES="$(cat secrets-heroku.properties)" "$@" --app {app_name}')
 
-  setVarsFromFile('javascript/.env.production',app_name)
-  setVarsFromFile('javascript/.env.local',app_name)
+  setVarsFromFile('javascript/.env.production',app_name,"(This is usually ok)")
+  setVarsFromFile('javascript/.env.local',app_name,"(WARNING: This is likely a problem!!)")
 
 if __name__=="__main__":
   main()
