79025a19f6b9a62cc874637fdc7b4472d5701f28
pconrad <pconrad@cs.ucsb.edu>
2021-03-12 17:51:06 +0000

    Deploying to main from  @ 8c0b9b67a02dfcb1447ce89d7f7950865e09fda2 🚀

---
+++ docs/storybook/precache-manifest.e8d578472a0e0c6a31982ac4757333bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff46cc0fe03691d8986beff06c86c59a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.28343df6408a856350b8.bundle.js"
+  },
+  {
+    "url": "runtime~main.28343df6408a856350b8.bundle.js"
+  },
+  {
+    "revision": "85801707cd36ac40a63a",
+    "url": "static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b0444464e7e5598240b1",
+    "url": "static/css/vendors~main.8aa5a7f8.chunk.css"
+  },
+  {
+    "url": "vendors~main.28343df6408a856350b8.bundle.js"
+  },
+  {
+    "url": "vendors~main.28343df6408a856350b8.bundle.js.LICENSE.txt"
+  }
+]);
