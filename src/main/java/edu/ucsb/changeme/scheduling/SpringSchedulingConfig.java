202ba03f2015b9eb7ed1f157c845ca85f346279c
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-03-12 14:47:12 -0800

    pc - add support for @Scheduled tasks

---
+++ src/main/java/edu/ucsb/changeme/scheduling/SpringSchedulingConfig.java
@@ -0,0 +1,23 @@
+package edu.ucsb.changeme.scheduling;
+
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ComponentScan("edu.ucsb.changeme.scheduling")
+@PropertySource("classpath:springScheduled.properties")
+public class SpringSchedulingConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
